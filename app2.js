68d5bb2e3d429911b0574b35dfa0de7ae413e861
CyrustheGreat03 <nnoori1@students.kennesaw.edu>
2024-11-05 13:01:05 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,28 +1,25 @@
+//express code is used for the node elements 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; // The port being used is 3000
 
-// 
 app.use(express.urlencoded({ extended: true }));
 
-// 
+//Used to send the contact HTML file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/contact.html');
 });
 
+//When the form submits, a message to the client-side from the server-side
 app.post('/submit-form', (req, res) => {
-    const FLname = req.body.FLname; 
-    const email = req.body.email; // access form data
-
+    const FLname = req.body.FLname; //Used to indicate Name and Email
+    const email = req.body.email; 
 
     res.send(`Thank you, ${FLname}. I will message you shortly at ${email}.`);
-    });
-
-
-// Thank you, ${FLname}. I will message you shortly at ${email}.
-
-// Start the server
+    }); 
+    //Sends a personal message and shows that request has been recieved
 
 app.listen(port, () => {
     console.log(`Server running on https://cyrusthegreat03.github.io/Project-Milestone-Two/${port}`);
-});
+}); 
+//Indicates which website the server should run on, using port 3000
